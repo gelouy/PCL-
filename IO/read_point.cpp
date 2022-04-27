8616d914cc626d0427f0e6dc973871fe8c77f697
阁楼 <46619044+gelouy@users.noreply.github.com>
2022-04-27 13:26:10 +0800

    Create read_point.cpp

---
+++ IO/read_point.cpp
@@ -0,0 +1,15 @@
+#include <pcl/io/pcd_io.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::io::loadPCDFile(argv[1],*cloud);
+
+	cout << "点云数量：" << cloud->size() << endl;
+
+	system("pause");
+	return 0;
+}
